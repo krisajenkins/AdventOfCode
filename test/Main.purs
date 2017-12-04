8664f8503c72007083adb6d1a93ac38517160365
Kris Jenkins <krisajenkins@gmail.com>
2017-12-04 22:57:31 +0000

    fixup! Day 4 complete.

--- test/Main.purs
+++ test/Main.purs
@@ -7,7 +7,9 @@ import Prelude
 import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
 import Year2017.Day3Test as Year2017.Day3Test
+import Year2017.Day4Test as Year2017.Day4Test
 
 main :: Eff (testOutput :: TESTOUTPUT, avar :: AVAR, console :: CONSOLE) Unit
 main = runTest do
   Year2017.Day3Test.all
+  Year2017.Day4Test.all
