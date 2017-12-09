1eb3a230e6956ac2793fac2d4f8371dabbb0d435
Kris Jenkins <krisajenkins@gmail.com>
2017-12-09 23:03:38 +0000

    Simplifying Day9 by giving Tree some common typeclass instances.

--- src/Year2017/Day9.purs
+++ src/Year2017/Day9.purs
@@ -5,8 +5,10 @@ import Control.Lazy (fix)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Data.Array as Array
-import Data.Foldable (sum)
+import Data.Foldable (class Foldable, fold, foldMap, foldl, foldr, sum)
 import Data.Generic (class Generic, gShow)
+import Data.String as String
+import Data.Traversable (class Traversable, sequenceDefault, traverseDefault)
 import Node.Encoding (Encoding(..))
 import Node.FS (FS)
 import Node.FS.Sync (readTextFile)
@@ -15,7 +17,6 @@ import Prelude hiding (between)
 import Text.Parsing.StringParser (Parser, runParser)
 import Text.Parsing.StringParser.Combinators (between, many, sepBy)
 import Text.Parsing.StringParser.String (anyChar, char, noneOf)
-import Data.String as String
 
 data Tree a
   = Node (Array (Tree a))
@@ -23,6 +24,22 @@ data Tree a
 
 derive instance genericTree :: Generic a => Generic (Tree a)
 derive instance eqTree :: Eq a => Eq (Tree a)
+
+instance functorTree :: Functor Tree where
+  map f (Leaf x) = Leaf (f x)
+  map f (Node xs) = Node (map f <$> xs)
+
+instance foldableTree :: Foldable Tree where
+  foldMap f (Leaf x) = f x
+  foldMap f (Node xs) = fold (foldMap f <$> xs)
+  foldr f b (Leaf a) = f a b
+  foldr f b (Node xs) = foldr (flip (foldr f)) b xs
+  foldl f b (Leaf a) = f b a
+  foldl f b (Node xs) = foldl (foldl f) b xs
+
+instance traversable :: Traversable Tree where
+  traverse = traverseDefault
+  sequence = sequenceDefault
 
 instance showTree :: (Generic a, Show a) => Show (Tree a) where
   show = gShow
@@ -66,5 +83,4 @@ solution2 :: forall eff. Eff (exception :: EXCEPTION, fs :: FS | eff) Int
 solution2 = garbageTotal <$> readInput
 
 garbageTotal :: Tree String -> Int
-garbageTotal (Leaf x) = String.length x
-garbageTotal (Node leaves) = sum (garbageTotal <$> leaves)
+garbageTotal = sum <<< map String.length
