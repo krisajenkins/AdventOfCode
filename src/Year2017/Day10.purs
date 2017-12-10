c32bb0ef7f4c8b9603e81fa0aacba4f4f4e406c5
Kris Jenkins <krisajenkins@gmail.com>
2017-12-10 20:10:21 +0000

    Day 10 done.

--- src/Year2017/Day10.purs
+++ src/Year2017/Day10.purs
@@ -2,17 +2,23 @@ module Year2017.Day10 where
 
 import Prelude
 
+import Control.Monad.Rec.Class (Step(Loop, Done), tailRec)
 import Control.Monad.State (class MonadState, execState)
 import Data.Array as Array
-import Data.Foldable (product)
+import Data.Char (toCharCode)
+import Data.Foldable (class Foldable, foldl, product)
 import Data.Generic (class Generic, gShow)
+import Data.Int (hexadecimal, toStringAs)
+import Data.Int.Bits (xor)
 import Data.Lens (Lens', modifying, use, (^.))
 import Data.Lens.Iso.Newtype (_Newtype)
 import Data.Lens.Record (prop)
 import Data.Newtype (class Newtype)
+import Data.String as String
 import Data.Symbol (SProxy(..))
 import Data.Traversable (class Traversable, traverse)
-import Utils (inc)
+import Data.Tuple.Nested ((/\))
+import Utils (dec, inc)
 
 newtype Rope a = Rope
   { items :: Array a
@@ -38,9 +44,9 @@ _skip = _Newtype <<< prop (SProxy :: SProxy "skip")
 
 twist :: forall a. Int -> Array a -> Array a
 twist n xs =
-  Array.drop n xs
-  <>
-  (Array.reverse (Array.take n xs))
+  Array.drop delta xs <> (Array.reverse (Array.take delta xs))
+  where
+    delta = mod n (Array.length xs)
 
 cycle :: forall a. Int -> Array a -> Array a
 cycle n xs =
@@ -57,32 +63,72 @@ step n = do
   modifying _skip inc
   use _distanceTravelled
 
+initialRope :: forall a. Array a -> Rope a
+initialRope xs = Rope { skip: 0, distanceTravelled: 0, items: xs }
+
+finaliseRope :: forall a. Rope a -> Array a
+finaliseRope finalState =
+  let finalItems = finalState ^. _items
+      len = Array.length finalItems
+      delta = len - (mod (finalState ^. _distanceTravelled) len)
+  in cycle delta finalItems
+
 runHash :: forall t. Traversable t => Array Int -> t Int -> Array Int
 runHash xs lengths =
-  let initialState = Rope { skip: 0, distanceTravelled: 0, items: xs }
-      finalState = execState
-                      (traverse step lengths)
-                      initialState
-      len = Array.length xs
-      delta = len - (mod (finalState ^. _distanceTravelled) len)
-  in cycle delta (finalState ^. _items)
+  finaliseRope $ execState
+                   (traverse step lengths)
+                   (initialRope xs)
+
 ------------------------------------------------------------
+
 input :: Array Int
 input = [94, 84, 0, 79, 2, 27, 81, 1, 123, 93, 218, 23, 103, 255, 254, 243]
 
 solution1 :: Int
 solution1 =
   product $ Array.take 2 $ runHash (Array.range 0 255) input
+
 ------------------------------------------------------------
 
-inputString :: String
-inputString = "94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243"
+xorBlock :: forall t. Foldable t => t Int -> Int
+xorBlock = foldl xor 0
+
+showHash :: forall t. Foldable t => t Int -> String
+showHash = foldl (\a -> append a <<< padLeft "0" <<< toStringAs hexadecimal) ""
+  where
+    padLeft padding xs =
+      case String.length xs of
+        0 -> padding <> padding
+        1 -> padding <> xs
+        _ -> xs
+
+knotHash :: String -> String
+knotHash str =
+  let input = Array.range 0 255
+      lengths = toLengths str <> salt
+      finalItems = finaliseRope $ tailRec go (64 /\ initialRope input)
+      go (0 /\ rope) = Done rope
+      go (n /\ rope) =
+        Loop (dec n /\ execState (traverse step lengths) rope)
+      xors = Array.take 16 (chunk 16 finalItems)
+      decs = xorBlock <$> xors
+      hash = showHash decs
+  in hash
+
+chunk :: Int -> Array Int -> Array (Array Int)
+chunk _ [] = [[]]
+chunk n xs =  Array.cons (Array.take n xs) (chunk n (Array.drop n xs))
+
+toLengths :: String -> Array Int
+toLengths xs =
+  toCharCode <$> String.toCharArray xs
 
 salt :: Array Int
 salt = [17, 31, 73, 47, 23]
 
-knotHash :: String -> String
-knotHash str = str
+inputString :: String
+inputString = String.joinWith "," $ show <$> input
 
 solution2 :: String
-solution2 = knotHash inputString
+solution2 =
+  knotHash inputString
--- test/Year2017/Day10Test.purs
+++ test/Year2017/Day10Test.purs
@@ -1,13 +1,9 @@
 module Year2017.Day10Test where
 
 import Prelude
-import Year2017.Day10
-
-import Control.Monad.Eff.Exception (EXCEPTION)
+import Year2017.Day10 (Rope(..), knotHash, showHash, solution1, solution2, step, toLengths, twist, xorBlock)
 import Control.Monad.State (evalState, execState)
-import Data.Array as Array
 import Data.Traversable (traverse)
-import Node.FS (FS)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert (equal)
 
@@ -16,6 +12,10 @@ all =
   suite "Day10" do
     twistTests
     stepTests
+    xorBlockTests
+    showHashTests
+    toLengthsTests
+    knotHashTests
     solution1Tests
     solution2Tests
 
@@ -38,9 +38,14 @@ stepTests =
          (traverse step [3, 4, 1, 5])
          (Rope { skip: 0, distanceTravelled: 0, items: [0, 1, 2, 3, 4] }))
     equal
-      (Rope { skip: 4, distanceTravelled: 19, items: [0, 3, 4, 2, 1] })
+      (Rope { skip: 4, distanceTravelled: 19, items: [2, 4, 3, 0, 1] })
       (execState
          (traverse step [3, 4, 1, 5])
+         (Rope { skip: 0, distanceTravelled: 0, items: [0, 1, 2, 3, 4] }))
+    equal
+      (Rope { skip: 1, distanceTravelled: 20, items: [0, 1, 2, 3, 4] })
+      (execState
+         (traverse step [20])
          (Rope { skip: 0, distanceTravelled: 0, items: [0, 1, 2, 3, 4] }))
 
 solution1Tests :: forall eff. TestSuite eff
@@ -48,10 +53,26 @@ solution1Tests =
   test "Solution 1" do
     equal 23715 solution1
 
+xorBlockTests :: forall eff. TestSuite eff
+xorBlockTests =
+  test "xorBlock" do
+    equal 64 (xorBlock [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22])
+
+showHashTests :: forall eff. TestSuite eff
+showHashTests =
+  test "showHash" do
+    equal "050cc8" (showHash [5, 12, 200])
+    equal "4007ff" (showHash [64, 7, 255])
+
+toLengthsTests :: forall eff. TestSuite eff
+toLengthsTests =
+  test "toLengths" do
+    equal [49, 44, 50, 44, 51]
+      (toLengths "1,2,3")
 
 knotHashTests :: forall eff. TestSuite eff
 knotHashTests =
-  test "Knot Hash" do
+  test "knotHash" do
     equal "a2582a3a0e66e6e86e3812dcb672a272" (knotHash "")
     equal "33efeb34ea91902bb2f59c9920caa6cd" (knotHash "AoC 2017")
     equal "3efbe78a8d82f29979031a4aa0b16a9d" (knotHash "1,2,3")
@@ -60,4 +81,4 @@ knotHashTests =
 solution2Tests :: forall eff. TestSuite eff
 solution2Tests =
   test "Solution 2" do
-    equal "" solution2
+    equal "541dc3180fd4b72881e39cf925a50253" solution2
