70991a8bfc5f388ac4c205cda80523671355fba3
Kris Jenkins <krisajenkins@gmail.com>
2017-12-11 20:43:14 +0000

    Adding a parser test for Day 11.

--- test/Year2017/Day11Test.purs
+++ test/Year2017/Day11Test.purs
@@ -2,17 +2,19 @@ module Year2017.Day11Test where
 
 import Prelude
 
-import Year2017.Day11 (Direction(..), distance, solution1, solution2, walk)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Exception (EXCEPTION)
+import Data.Foldable (length)
 import Node.FS (FS)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert (equal)
+import Year2017.Day11 (Direction(..), distance, readInput, solution1, solution2, walk)
 
 all :: forall eff. TestSuite (fs :: FS, exception :: EXCEPTION | eff)
 all =
   suite "Day11" do
     distanceTests
+    readInputTests
     solution1Tests
     solution2Tests
 
@@ -23,6 +25,12 @@ distanceTests =
     equal 0 $ distance $ walk [NE, NE, SW, SW]
     equal 2 $ distance $ walk [NE, NE, S, S]
     equal 3 $ distance $ walk [SE, SW, SE, SW, SW]
+
+readInputTests :: forall eff. TestSuite (fs :: FS, exception :: EXCEPTION | eff)
+readInputTests =
+  test "readInput" do
+    answer <- liftEff readInput
+    equal 8223 (length answer)
 
 solution1Tests :: forall eff. TestSuite (fs :: FS, exception :: EXCEPTION | eff)
 solution1Tests =
